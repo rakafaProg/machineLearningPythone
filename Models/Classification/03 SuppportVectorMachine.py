a224790306df2f4f4c683c9297d0b4b8d9533d92
Rakefet Ifrach <rakefetifrach@workiz.com>
2019-03-04 16:01:39 +0200

    start with classifications

---
+++ Models/Classification/03 SuppportVectorMachine.py
@@ -0,0 +1,124 @@
+# Support Vector Machine (SVM)
+
+# Logistic Regression
+
+# math tools
+import numpy as np
+# to plot charts
+import matplotlib.pyplot as plt
+# import and manage dataset
+import pandas as pd
+
+""" This model is better in more dimansions """
+
+# ===============================
+#     Importing the data set
+# ===============================
+
+fileName = 'Social_Network_Ads'
+featuresLength = 4
+baseUrl = '/Users/rakefetyifrach/Desktop/python-machine-learning/DataExamples/'
+
+# Importing the dataset
+dataset = pd.read_csv(baseUrl + fileName + '.csv')
+
+# Create matrix of the features
+X = dataset.iloc[:, 2:4].values
+# Create vector of values
+y = dataset.iloc[:, featuresLength].values
+
+
+# After importing - clear unused variables
+del fileName
+del baseUrl
+del featuresLength
+
+
+
+# ===============================
+#    Split to train and test
+# ===============================
+
+testSize = 0.2
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = testSize, random_state = 0)
+
+del testSize
+
+
+# ===============================
+#        Feature Scaling
+# ===============================
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+# Fit only on train set 
+# so train and test will be scaled on the same basis 
+"""sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))
+y_test = sc_y.transform(y_test.reshape(-1,1))"""
+
+
+# ===============================
+#        Fit the model
+# ===============================
+# Fitting Classifier to the dataset
+from sklearn.svm import SVC
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train,y_train)
+
+# Predicting on test data
+y_pred = classifier.predict(X_test)
+
+# Compute confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# ===============================
+#      Visualise the results
+# ===============================
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+# arrange color pixels
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# delete not used variables
+del i,j,y,X_set,y_set,X1,X2
+    
